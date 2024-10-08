9e97f8f8b84604dfd49a16433b7162cb267787ce
Lior1254 <liorshemtov2008@gmail.com>
2024-10-08 15:19:25 +0300

    s

--- src/Phone.java
+++ src/Phone.java
@@ -3,6 +3,6 @@ public class Phone implements Chargeable
     @Override
     public void charge()
     {
-        System.out.println("Charging Phone");
+        System.out.println("Charging Phone!!!");
     }
 }
